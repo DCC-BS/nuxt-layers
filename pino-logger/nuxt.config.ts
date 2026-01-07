8e6ad03f00a13deec4ff8a2c355fcba1ae2ba5c7
Tobias Bollinger <tobias.bollinger@gmail.com>
2026-01-07 14:35:17 +0100

    Remove Nuxt layer extension logic

--- pino-logger/nuxt.config.ts
+++ pino-logger/nuxt.config.ts
@@ -1,17 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import type { SourceOptions } from "c12";
-
-function getExtends() {
-    const layers: Array<[string, SourceOptions?]> = [];
-
-    if (process.env.LOGGER_LAYER_URI) {
-        layers.push([process.env.LOGGER_LAYER_URI, { install: true }]);
-    }
-
-    return layers;
-}
-
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    extends: getExtends(),
 });
