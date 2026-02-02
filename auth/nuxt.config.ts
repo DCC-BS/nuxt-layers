04b08223b99a0851fce9c7ad266fe73096ceb329
Tobias Bollinger <tobias.bollinger@gmail.com>
2026-02-02 17:03:57 +0100

    fix: add install: true to layer configurations for proper dependency installation

--- auth/nuxt.config.ts
+++ auth/nuxt.config.ts
@@ -4,11 +4,11 @@ function getExtends() {
     const layers: Array<[string, SourceOptions?]> = [];
 
     if (process.env.AUTH_LAYER_URI) {
-        layers.push([process.env.AUTH_LAYER_URI]);
+        layers.push([process.env.AUTH_LAYER_URI, { install: true }]);
     }
 
     layers.push([
-        "github:DCC-BS/nuxt-layers/backend_communication"
+        "github:DCC-BS/nuxt-layers/backend_communication", { install: true }
     ]);
 
     return layers;
