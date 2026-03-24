5565f75fe1ee76b9c1cf1c501459fcccfc9b1b55
Tobias Bollinger <tobias.bollinger@bs.ch>
2026-03-24 15:43:06 +0100

    wip

--- azure-auth/server/utils/authHandler.ts
+++ azure-auth/server/utils/authHandler.ts
@@ -2,6 +2,8 @@ export const authHandler = backendHandlerBuilder().extendFetchOptions(
     async (options) => {
         const { apiAccessToken } = await getAuthContext(options.event);
 
+        console.log("access token is", apiAccessToken);
+
         return {
             ...options,
             headers: {
